fc00780fce18245979bf2c79da9f44eb07feeda7
Shuucroute <ethan.rampnoux@gmail.com>
2024-03-08 11:52:06 +0100

    Ne rien casser s'il vous plait

---
+++ Classes.js
@@ -0,0 +1,23 @@
+function redirectToCPPage() {
+    window.location.href = "CP/Cp-matiere.html";
+}
+
+function redirectToCE1Page() {
+    window.location.href = "CE1/CE1-matiere.html";
+}
+
+function redirectToCE2Page() {
+    window.location.href = "CE2/CE2-matiere.html";
+}
+
+function redirectToCM1Page() {
+    window.location.href = "CM1/CM1-matiere.html";
+}
+
+function redirectToCM2Page() {
+    window.location.href = "CM2/CM2-matiere.html";
+}
+
+function redirectToConnexionPage() {
+    window.location.href = "Page_de_connexion/Connexion.html";
+}

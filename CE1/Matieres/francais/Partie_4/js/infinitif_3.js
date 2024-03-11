feb20308d63ada574377d17804b87406161e983e
Shuucroute <ethan.rampnoux@gmail.com>
2024-03-11 19:14:36 +0100

    suite francais

---
+++ CE1/Matieres/francais/Partie_4/js/infinitif_3.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var syllabesCorrectes = ['Regarder', 'Choisir', 'Construire', 'Chanter'];
+
+    document.querySelector('.btn_valider').addEventListener('click', function () {
+        var reponses = document.querySelectorAll('.btn_start');
+        var reponseCoSelectionnee = false;
+        var reponsesSelectionnees = [];
+
+        reponses.forEach(function (reponse) {
+            if (reponse.classList.contains('selected')) {
+                reponsesSelectionnees.push(reponse.textContent);
+
+                if (reponse.textContent === 'Choisir') {
+                    reponseCoSelectionnee = true;
+                }
+            }
+        });
+
+        var resultat = document.createElement('p');
+
+        if (reponseCoSelectionnee) {
+            resultat.textContent = 'Bonne réponse !';
+
+            window.location.href = 'infinitif_4.html';
+        } else {
+            resultat.textContent = 'Mauvaise réponse. Réessayez.';
+            resultat.style.color = 'red'; 
+            document.querySelector('.container').appendChild(resultat);
+        }
+
+        document.querySelector('.start-screen').appendChild(resultat);
+    });
+
+    document.querySelectorAll('.btn_start').forEach(function (reponse) {
+        reponse.addEventListener('click', function () {
+            document.querySelectorAll('.btn_start').forEach(function (btn) {
+                btn.classList.remove('selected');
+            });
+
+            // Ajoute la classe 'selected' au bouton cliqué
+            reponse.classList.add('selected');
+        });
+    });
+});
---
+++ CE1/Matieres/francais/Partie_5/js/negation_1.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var syllabesCorrectes = ['Je ne prends pas de dessert.', 'Je ne mange pas.', "Je n'aime pas les brocolis.", 'Je ne veux pas de légumes.'];
+
+    document.querySelector('.btn_valider').addEventListener('click', function () {
+        var reponses = document.querySelectorAll('.btn_start');
+        var reponseCoSelectionnee = false;
+        var reponsesSelectionnees = [];
+
+        reponses.forEach(function (reponse) {
+            if (reponse.classList.contains('selected')) {
+                reponsesSelectionnees.push(reponse.textContent);
+
+                if (reponse.textContent === 'Je ne mange pas.') {
+                    reponseCoSelectionnee = true;
+                }
+            }
+        });
+
+        var resultat = document.createElement('p');
+
+        if (reponseCoSelectionnee) {
+            resultat.textContent = 'Bonne réponse !';
+
+            window.location.href = 'negation_2.html';
+        } else {
+            resultat.textContent = 'Mauvaise réponse. Réessayez.';
+            resultat.style.color = 'red'; 
+            document.querySelector('.container').appendChild(resultat);
+        }
+
+        document.querySelector('.start-screen').appendChild(resultat);
+    });
+
+    document.querySelectorAll('.btn_start').forEach(function (reponse) {
+        reponse.addEventListener('click', function () {
+            document.querySelectorAll('.btn_start').forEach(function (btn) {
+                btn.classList.remove('selected');
+            });
+
+            // Ajoute la classe 'selected' au bouton cliqué
+            reponse.classList.add('selected');
+        });
+    });
+});

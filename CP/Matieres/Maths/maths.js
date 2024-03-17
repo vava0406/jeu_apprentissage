d79977cdff5f312b1b08677930e839b4e1fd0d52
Shuucroute <ethan.rampnoux@gmail.com>
2024-03-17 10:36:25 +0100

    continuation

--- CP/Matieres/Maths/maths.js
+++ CP/Matieres/Maths/maths.js
@@ -1,27 +1,23 @@
-function redirectToAEPage() {
-    window.location.href = "Partie_1/AE_1.html";
+function redirectToAdditionPage() {
+    window.location.href = "Partie_1/Addition_1.html";
 }
 
 function redirectToCroissantPage() {
     window.location.href = "Partie_2/Croissant_1.html";
 }
 
-function redirectToDecroissantPage() {
-    window.location.href = "Partie_3/Decroissant_1.html";
+function redirectToPlusGrandPage() {
+    window.location.href = "Partie_3/PG_1.html";
 }
 
-function redirectToNAPPage() {
-    window.location.href = "Partie_4/NAP_1.html";
+function redirectToPlusPetitPage() {
+    window.location.href = "Partie_4/Inferieur_1.html";
 }
 
-function redirectToNAVPage() {
-    window.location.href = "Partie_5/NAV_1.html";
-}
-
-function redirectToCalculPage() {
-    window.location.href = "Partie_6/Calcul_1.html";
+function redirectToDoublePage() {
+    window.location.href = "Partie_5/Double_1.html";
 }
 
 function redirectToIndexPage() {
-    window.location.href = "../../CE1_matiere.html";
+    window.location.href = "../../CP_matiere.html";
 }

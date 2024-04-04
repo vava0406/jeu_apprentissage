c498220d2c8dbeca29ebcc9ecfb93de6affce0f0
Shuucroute <ethan.rampnoux@gmail.com>
2024-04-04 11:09:30 +0200

    test

---
+++ CM2/Matieres/anglais/Partie_1/php/process.php
@@ -0,0 +1,38 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['answer'])) {
+        $reponse = $_POST['answer'];
+
+        $page = $_GET['page'];
+
+        $reponse_correcte = $_POST['reponse_correcte'];
+
+        // Connexion à la base de données MySQL
+        $servername = "localhost";
+        $username = "votre_nom_utilisateur";
+        $password = "votre_mot_de_passe";
+        $dbname = "nom_de_votre_base_de_donnees";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO reponses (reponse, page, reponse_correcte) VALUES ('$reponse', '$page', '$reponse_correcte')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Réponse ajoutée avec succès à la base de données.";
+        } else {
+            echo "Erreur: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    } else {
+        echo "Aucune réponse sélectionnée.";
+    }
+} else {
+    echo "Erreur: Méthode de requête incorrecte.";
+}
+?>

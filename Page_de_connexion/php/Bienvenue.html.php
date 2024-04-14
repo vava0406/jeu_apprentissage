0a8bf4ebef920e7096b33012ea4187f1fcdbb9a0
vava0406 <evaguillou04@gmail.com>
2024-04-14 12:40:08 +0200

    reparation

--- Page_de_connexion/php/Bienvenue.html.php
+++ Page_de_connexion/php/Bienvenue.html.php
@@ -7,15 +7,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/Bienvenue.css">
+    <link rel="stylesheet" href="../css/Bienvenue.css">
     <title>Bienvenue</title>
-
 </head>
 <body>
 
     <div class="container">
 
-        <h1 class="heading"> Vous êtes inscrit! </h1> 
+        <h1 class="heading"> Vous êtes inscrit!</h1> 
 
         <div class="heading2">
             <?php if (isset($error)) {
@@ -23,7 +22,7 @@
             } else if (isset($result)) {
                 echo 'Bienvenue parmis nous ' .$prenom.'.';
             }?>
-        <h4><a class="btn_connexion" href="Connexion.html">Connexion</a></h4>
+            <h4><a class="btn_connexion" href="../Connexion.html">Connexion</a></h4>
 
         </div>
 

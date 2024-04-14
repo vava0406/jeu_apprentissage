6e1d86ed95ffbcbe714a1587b586c6f470885a81
vava0406 <evaguillou04@gmail.com>
2024-04-14 12:27:50 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -18,14 +18,14 @@ $utilisateur = $_SESSION['utilisateur'];
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="accueil.css">
     <title>Accueil</title>
 </head>
 <body>
     <div class="container">
         <h1 class="heading">Bonjour <?php echo htmlspecialchars($utilisateur['Prenom']); ?> !</h1>
         <p class="heading2">Vous êtes connecté en tant que <?php echo htmlspecialchars($utilisateur['Email']); ?>.</p>
-        <h4><a class="btn_accueil" href="Accueil.html">Accueil</a></h4>
+        <h4><a class="btn_accueil" href="index.html.php">Accueil</a></h4>
         <a href="deconnexion.php" class="btn_deconnexion">Déconnexion</a>
     </div> 
 
